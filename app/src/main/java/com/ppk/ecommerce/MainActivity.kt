b5ce5e090c573b72cd37e9e5d4962a82d6ee91b3
phoopwint169 <mkt.ucsy@gmail.com>
2019-11-05 12:03:33 +0630

    Mes

--- app/src/main/java/com/ppk/ecommerce/MainActivity.kt
+++ app/src/main/java/com/ppk/ecommerce/MainActivity.kt
@@ -2,11 +2,13 @@ package com.ppk.ecommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("mes","Hello")
     }
 }
